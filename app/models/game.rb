8d4b2dc45443eb4b38b8dda64d0d656c2d017b22
Bishwa Hang Rai <bishwahang.kirat@gmail.com>
2016-02-22 00:12:12 +0100

    some comments

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,11 +1,13 @@
 class Game < ActiveRecord::Base
   has_many :frames, -> { order "created_at ASC" }
+
   def finished?
     return false if self.frames.count < 10
     return false if extra_frame?
     true
   end
 
+  # if the 11th frame is required?
   def extra_frame?
     if self.frames.count == 10
       last_frame = frames.last
@@ -19,9 +21,14 @@ class Game < ActiveRecord::Base
     frames.take(10).map(&:score).inject(:+)
   end
 
+  # loops through the frames created so far
+  # updates the score for strike and spare frame
   def update_score
     frames.take(10).each_with_index do |frame, index|
+      # do not consider any frame that is already marked done
+      # or the extra frame (11th frame)
       unless frame.mark || frame.extra_frame
+        # for all frame besides the 10th frame
         if index < 9
           # if frame is strike check for two consequetive ball
           if frame.is_strike? && frames[index.succ]
@@ -43,7 +50,7 @@ class Game < ActiveRecord::Base
             frame.update_and_mark!(score: points)
           end
         else
-          # byebug
+          # for the last frame (10th frame)
           if frames[index.succ]
             next_frame = frames[index.succ]
             points = 10 + next_frame.total_score?
